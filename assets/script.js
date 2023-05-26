7b9b0217c22164eb1a739c37fe2efaa737238877
Zombear323 <129801201+Zombear323@users.noreply.github.com>
2023-05-26 16:08:05 -0400

    edits

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var Peter = "peter"
+
+console.log("text test " + Peter)
+console.log(typeof Peter)
+
+var cheesez = ["mozza", "chedda", "parmazan", "goudah"]
+
+for (let index = 0; index < cheesez.length; index++) {
+    const element = cheesez[index];
+    console.log("yummy " + cheesez[index])
+}
